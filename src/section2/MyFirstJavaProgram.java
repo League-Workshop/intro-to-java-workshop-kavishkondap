3581cec7d6f4a25ed62d3763457beaa459150094
kavishkondap <65979777+kavishkondap@users.noreply.github.com>
2020-06-23 19:01:50 +0000

    Add files via upload
    Day 2 Work

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,28 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
+	Robot Bob = new Robot();
+//		Bob.penDown();
+//	Bob.move(200);
+//		Bob.turn(180);
+//		Bob.move(100);
+//		Bob.turn(-90);
+//		Bob.move(100);
+//		Bob.turn(90);
+//		Bob.move(100);
+//		Bob.turn(180);
+//		Bob.move(200);
+//		Bob.penUp();
+//		Bob.turn(180);
+//		Bob.move(100);
+//		Bob.turn(-90);
+//		Bob.move(50);
 		
+for (int i = 0; i < 100000; i++) {
+	Bob.penDown();
+	Bob.turn(1);
+	Bob.move(1);
+}
 		
 	}
 }
