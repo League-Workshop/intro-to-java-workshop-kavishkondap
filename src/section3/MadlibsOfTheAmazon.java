7f373e1da92c0bc8a0abc6f0e1d544364bed8f8c
root <root@54cb13eac0f6>
2020-06-24 18:57:30 +0000

    Day 3 Work

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -7,17 +7,17 @@ public class MadlibsOfTheAmazon {
 	public static void main(String[] args) {
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
-		
+		JOptionPane.showMessageDialog(null, "If you find yourself having to cross a piranha-infested river, here's how to do it...");
 		// Get the user to enter an adjective
-
+String adjective = JOptionPane.showInputDialog(null,"Enter an adjective");
 		// Get the user to enter a type of liquid
-
+String liquid = JOptionPane.showInputDialog(null,"Enter a liquid");
 		// Get the user to enter a body part
-
+String body =JOptionPane.showInputDialog(null,"Enter a body part");
 		// Get the user to enter a verb
-
+String verb =JOptionPane.showInputDialog(null,"Enter a verb");
 		// Get the user to enter a place
-
+String place =JOptionPane.showInputDialog(null,"Enter a place");
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
@@ -26,8 +26,12 @@ public class MadlibsOfTheAmazon {
 		// back to the [place]. Good luck!
 		
 		// Make a pop-up for the final story. You can use \n to go to the next line
+		String Madlib = "Piranhas are more " + adjective +" during the day, so cross the river \n"
++ " at night. Prianhas are attracted to fresh " + liquid + " and will most likely take a bite out \n"
++ "of your " + body + " if you " + verb + " . Whatever you do, if you have an open wound, try to find \n"
++ "another way to get back to the " + place + " . Good Luck!";
 		
-
+		JOptionPane.showMessageDialog(null,Madlib);
 	}
 }
 
--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -12,29 +12,35 @@ public class DragonFight {
 		
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+	int playerHealth = 100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+		int dragonHealth = 100;
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+		int playerdamage = 10;
 		// 5. Create a variable to hold the damage the dragon's attack does each round
-		
+		int dragondamage = 25;
 		
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
+		while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
 		
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
-		
+		Random randy = new Random ();
 		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
+		String answer = JOptionPane.showInputDialog(null,"Attack the dragon ith a kick or a yell?");
 		// 9. If they typed in "yell":
-		
+		if (answer.equals("yell")) {
+			dragondamage = randy.nextInt(10+1);
+			dragonHealth -= dragondamage;
+		}
 			//-- Find a random number between 0 and 10 and store it in dragonDamage
-			
+		
 			//-- Subtract that number from the dragon's health variable 
 			
 		// 10. If they typed in "kick":
-		
+		else {
+			dragondamage = randy.nextInt (25+1);
+			dragonHealth -= dragondamage;
+		}
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
 			
 			//-- Subtract that number from the dragon's health variable
@@ -42,24 +48,32 @@ public class DragonFight {
 			
 		
 		// 11.  Find a random number between 0 and 35 and store it in playerDamage
-		
+		playerdamage = randy.nextInt (35+1);
 		// 12. Subtract this number from the player's health
 		
-		
+		playerHealth -= playerdamage;
 		
 		// 13. If the user's health is less than or equal to 0
 		
 			//-- Tell the user that they lost
-			
+			if (playerHealth <= 0 ) {
+				JOptionPane.showMessageDialog(null, "You Lost");
+			}
 		
 		// 14. Else if the dragon's health is less than or equal to 0
-		
+			else if (dragonHealth <= 0) {
+				JOptionPane.showMessageDialog(null, "You Won, and stole the Dragon's gold!");
+			}
 			//-- Tell the user that the dragon is dead and they took a ton of gold!
 			
 	   //  15.  Else
-			
+			else {
+				JOptionPane.showMessageDialog(null, "Knight Health = " + playerHealth + "Dragon Health = " + dragonHealth + " Damage dealt by knight = " + dragondamage + " Damage dealt by dragon = " + playerdamage);
+			}
 			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
 			
 		}
+		}
+		
 	}
 
